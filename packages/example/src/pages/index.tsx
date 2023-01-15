91f3f6a9140e1bc90c53fbe3d387853c821a86d9
yusixian <3450414733@qq.com>
2023-01-16 00:02:19 +0800

    fix: 修改入口按钮地址

--- packages/example/src/pages/index.tsx
+++ packages/example/src/pages/index.tsx
@@ -15,8 +15,8 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+          <Link className="button button--secondary button--lg" to="/docs/guide">
+            快速开始 ⏱️
           </Link>
         </div>
       </div>
