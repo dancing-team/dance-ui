e197fede696d81fa6ed02102072eabbf1a1657f9
yusixian <3450414733@qq.com>
2023-01-16 21:22:39 +0800

    fix: post css导致的打包报错

--- packages/components/postcss.config.cjs
+++ packages/components/postcss.config.cjs
@@ -1,10 +1,6 @@
-const path = require('path')
-
 module.exports = {
   plugins: {
-    tailwindcss: {
-      config: path.join(__dirname, 'tailwind.config.js'),
-    },
+    tailwindcss: {},
     autoprefixer: {},
   },
 }
