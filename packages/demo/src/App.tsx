734e4d140cad796f382c219f0936107fd76f27f6
yusixian <3450414733@qq.com>
2023-01-16 23:18:36 +0800

    fix: remove Button focus outline

--- packages/demo/src/App.tsx
+++ packages/demo/src/App.tsx
@@ -28,7 +28,9 @@ const App = (): ReactElement => {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-        <Button size="big">111</Button>
+        <Button onClick={() => alert('hello')} size="big">
+          test
+        </Button>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
