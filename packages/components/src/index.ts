dce31a443cd0d9b95e9561071fcd2bc2aa6fa08e
rykerFeng <2279549769@qq.com>
2023-02-07 19:22:06 +0800

    feat: 新建 tag 分支

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,2 +1,11 @@
+/*
+ * @Author: rykerFeng 2279549769@qq.com
+ * @Date: 2023-01-22 15:55:51
+ * @LastEditors: rykerFeng 2279549769@qq.com
+ * @LastEditTime: 2023-02-07 19:01:56
+ * @FilePath: /dance-ui/packages/components/src/index.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import './globals.css'
 export { default as Button } from './Button'
+export { default as Tag } from './Tag'
