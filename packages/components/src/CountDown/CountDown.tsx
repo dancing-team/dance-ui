0291fc77c8018980c7aab41e3cc5013a70763e0d
vlinglandy <1747024205@qq.com>
2023-02-10 21:45:52 +0800

    xxx

--- packages/components/src/CountDown/CountDown.tsx
+++ packages/components/src/CountDown/CountDown.tsx
@@ -1,79 +1,26 @@
 import classNames from 'classnames'
-import React, { LegacyRef } from 'react'
+import React, { LegacyRef, useEffect, useState } from 'react'
+import { useMount } from 'react-use'
 
 export type CountDownProps = {
-  /** 按钮类型 */
-  type?: 'default' | 'primary' | 'link' | 'unstyle'
-  /** 按钮大小 */
-  size?: 'large' | 'middle' | 'small'
-  /** 点击事件 */
-  onClick?: () => void
-  /** 是否为危险按钮（红色警告） */
-  danger?: boolean
-  /** 是否为幽灵按钮 */
-  ghost?: boolean
-  /** 是否禁用 */
-  disabled?: boolean
-  /** 是否加载中 */
-  loading?: boolean
-  /** 组件额外的 CSS className */
-  className?: string
-  /** 组件额外的 CSS style */
-  style?: React.CSSProperties
-}
-const typeClass = {
-  default: 'bg-white border-gray-500 hover:border-blue-500 hover:text-blue-500',
-  primary: 'bg-blue-500 border-blue-500 text-white hover:bg-blue-400 hover:border-blue-400',
-  link: 'border-transparent hover:text-blue-500',
-}
-const ghostClass = {
-  default: 'border-white text-white hover:border-blue-500 hover:text-blue-500',
-  primary: 'bg-transparent text-blue-500 border-blue-500 hover:text-blue-400 hover:border-blue-400',
-  link: 'border-transparent text-white hover:text-blue-500',
+  time?: Number | String,
+  size?: Number | String,
+
 }
 
-const dangerClass = {
-  default: 'text-red-500 border-red-500 hover:border-red-400 hover:text-red-400',
-  primary: 'bg-red-500 text-[#fff] border-red-500 hover:bg-red-400 hover:text-[#fff] hover:border-red-400',
-  link: 'text-red-500 hover:text-red-400',
-}
-const sizeClass = {
-  large: 'py-2 px-5',
-  middle: 'py-1 px-4',
-  small: 'px-1',
-}
-const CountDown = React.forwardRef(function ButtonInner(
-  { type, size, className, onClick, disabled, danger, ghost, loading, style, children }: React.PropsWithChildren<CountDownProps>,
-  ref: LegacyRef<HTMLButtonElement>,
-) {
+const CountDown: React.FC<CountDownProps> = (props: CountDownProps): JSX.Element => {
+  let [time, setTime] = useState(props.time)
+  setTimeout(() => {
+    setTime(time - 1)
+  }, 1000);
   return (
-    <div>
-       <button
-        className={
-          type === 'unstyle'
-            ? className
-            : classNames(
-                'box-border border transition focus:outline-none',
-                sizeClass[size ?? 'middle'],
-                danger ? dangerClass[type ?? 'default'] : null,
-                ghost ? ghostClass[type ?? 'default'] : typeClass[type ?? 'default'],
-                className,
-              )
-        }
-        style={style}
-        onClick={loading ? undefined : onClick}
-        ref={ref}
-        disabled={disabled}>
-        {/* <Loading show={loading} className="mr-2" /> */}
-        {children}
-      </button>
-      <div>CountDow</div>
-    </div>
+    <div>{time}</div>
   )
-})
+}
+
+
 CountDown.defaultProps = {
-  type: 'default',
-  size: 'middle',
-  loading: false,
+  time: 0,
+  size: 12
 }
 export default CountDown
